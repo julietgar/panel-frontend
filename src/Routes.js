5fc74bc606f0246d53ceb6a3cf6a10bc941d2e13
Juliet Garcia <julietgar.duque@gmail.com>
2022-03-26 07:59:08 -0300

    main structure added

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Panel from "../views/Panel";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Panel from "./views/Panel";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const Routes = () => {
+const MainRoutes = () => {
   return (
-    <>
-      <Router>
-          <Route exact path="/" component={Panel} />
-      </Router>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Panel />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
-export default Routes;
+export default MainRoutes;
--- src/views/Panel.jsx
+++ src/views/Panel.jsx
@@ -1,120 +1,69 @@
 import React from "react";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { Link } from "react-router-dom";
-import Panel from "../components/Panel";
+//import PanelComp from "../components/Panel";
+import { Container, Col, Row, Navbar, Nav, Table } from 'react-bootstrap';
 
 const Panel = () => {
-  var CurrentComponent = Panel;
   return (
     <>
-      <Tabs>
-        <TabList>
-          {/* START LEFT MENU CONTENT */}
-          <div className="leftpart">
-            <div className="leftpart_inner">
-              <div className="logo">
-                <Link className="navbar-brand" to="/">
-                  <img src="/assets/img/logo/julietgar-logo.jpg" alt="brand" />
-                </Link>
-              </div>
-              {/* END LOGO */}
+    {/* Main title */}
+    <Row lg={12}>
+      <Col><h1>Machine 1 Dashboard</h1></Col>
+    </Row>
 
-              <div className="menu">
-                <ul>
-                  <Tab>
-                    <img
-                      className="svg"
-                      src="/assets/img/svg/home-run.svg"
-                      alt="homerun"
-                    />
-                    <span className="menu_content">Home</span>
-                  </Tab>
-                  <Tab>
-                    <img
-                      className="svg"
-                      src="/assets/img/svg/avatar.svg"
-                      alt="avatar"
-                    />
-                    <span className="menu_content">About</span>
-                  </Tab>
-                  <Tab>
-                    <img
-                      className="svg"
-                      src="/assets/img/svg/briefcase.svg"
-                      alt="briefcase"
-                    />
-                    <span className="menu_content">Projects</span>
-                  </Tab>
-                  <Tab>
-                  <a 
-                    className="link-tab-decoration" 
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://drive.google.com/drive/folders/0B0DqzwjPAhtYdW1xbjVsdWdoYUU?resourcekey=0-V0eH397m0KLXhsETnGyniQ">
-                  
-                      <img
-                        className="svg"
-                        src="/assets/img/svg/document.svg"
-                        alt="avatar"
-                      />
-                      <span className="menu_content">Resume</span>
-                      
-                    </a>
-                  </Tab>
-                </ul>
-              </div>
-              {/* END MENU */}
+    {/* Options */}
+    <Row lg={12} className="mt-3">
+      <Col>
+        <Navbar bg="light" variant="light">
+          <Container>
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+          </Container>
+        </Navbar>
+      </Col>
+    </Row>
 
-              <div className="copyright">
-                <p>
-                  &copy; {new Date().getFullYear()} Juliet Garcia - @julietgar
-                </p>
-              </div>
-              {/* END COPYRIGHT */}
-            </div>
-          </div>
-          {/* END LEFT MENU CONTENT */}
-        </TabList>
-        {/* END SIDEBAR TABLIST */}
+    {/* Second title */}
+    <Row lg={12} className="mt-5 justify-content-md-center">
+      <Col md="auto"><h3>ON - LOADED</h3></Col>
+    </Row>
 
-        {/* START RIGHT PART CONTENT */}
-        <div className="rightpart">
-          <div className="rightpart_in">
-            <div className="tokyo_tm_section">
-              <div className="container">
-                <TabPanel>
-                  <div data-aos="fade-right" data-aos-duration="1200">
-                    <Home />
-                  </div>
-                </TabPanel>
-                {/* END HOME MENU TAB CONTENT */}
-
-                <TabPanel>
-                  <div
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="100"
-                  >
-                    <About />
-                  </div>
-                </TabPanel>
-                {/* END ABOUT MENU TAB CONTENT */}
-
-                <TabPanel>
-                  <Portfolio />
-                </TabPanel>
-                {/* END PORTFOLIO MENU TAB CONTENT */}
-
-                <TabPanel>
-                  <CurrentComponent />
-                </TabPanel>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* END RIGHT PART CONTENT */}
-      </Tabs>
-      {/* END TABS */}
+    {/* Table */}
+    <Row lg={12} className="mt-5">
+      <Col>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan={2}>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
     </>
   );
 };
